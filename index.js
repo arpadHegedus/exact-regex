9cd0f87a09c17b1d49d668e81e73237267f96ada
Arpad.Hegedus <hegedus.arpad@gmail.com>
2017-02-16 12:45:19 +0000

    Fixed huge mistake (accidentally added wrong index.js)

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
-var postcss         =   require('postcss'),
-    overwrite       =   require('overwrite-object'),
-    parser          =   require('postcss-value-parser');
-
-module.exports = postcss.plugin('postcss-grid', function(opt) {
-    return function(css) {
-        var defaultOpt = {
-            wrap: {
-                normal: {
-                    width: '1200px',
-                    padding: {
-                        horizontal: '1rem'
-                    }
-                }
-            },
-        };
-        function loop(rule) {
-            // clear: fix
-            rule.walkDecls('clear', function(decl) {
-                if(decl.value == 'fix') {
-                    css.insertBefore(rule, `
-                        ${rule.selector}:after {
-                            content: ' ';
-                            display: table;
-                            clear: both;
-                        }
-                    `);
-                    decl.remove(); if(rule.nodes.length == 0) { rule.remove(); }
-                }                
-            });
-            // wrap
-            rule.walkDecls('wrap', function(decl) {
-                console.log(parser(decl.value));
-                decl.remove();
-            });
-        }
-        css.walkRules(function(rule) {
-            loop(rule);
-        });
-    }
-});
+module.exports = (val, regex) => {
+    if(typeof val !== 'string') { return false; }
+    val = val.trim();
+    let matches = val.match(regex);
+    if(!matches) { return false; }
+    if(matches[0] !== val) { return false; }
+    return true;
+}
